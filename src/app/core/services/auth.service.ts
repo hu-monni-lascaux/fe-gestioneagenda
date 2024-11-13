88bec638414a3227c6bfdb637c0b12624568d24c
Stefano Laptop <stefanohu926@gmail.com>
2024-11-13 15:19:25 +0100

    jwt funziona

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -9,7 +9,7 @@ import {jwtDecode} from "jwt-decode";
 })
 export class AuthService {
     #token?: string;
-    #apiUrl: string = "http://localhost:8081/api/v1/auth"
+    #apiUrl: string = "http://localhost:8080/api/v1/auth"
 
     constructor(private http: HttpClient) {
     }
